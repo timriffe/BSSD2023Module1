661854d49255275b220be6ab09af003f2e5f96a8
timriffe <tim.riffe@gmail.com>
2023-07-06 09:23:22 +0200

    improve lifetable function to account for column closeout values, where necessary

--- my_functions.R
+++ my_functions.R
@@ -38,3 +38,53 @@ Txlx_to_ex <- function(Tx, lx){
   ex <- Tx / lx
   return(ex)
 }
+
+# version of the lifetable that uses vector
+# arguments; 
+# 6 July
+my_lifetable <- function(age,mx,ax){
+  
+  # ax closeout, use 1/mx
+  # that is, the mean of a constant hazard 
+  n     = length(mx)
+  ax[n] = 1/mx[n]
+  
+  qx    = mx_to_qx(mx = mx, ax = ax)
+  lx    = qx_to_lx(qx, radix = 1)
+  dx    = lxqx_to_dx(lx,qx)
+  Lx    = lxdx_to_Lx(lx,ax,dx)
+  
+  # Lx needs thoughtful closeout too
+  Lx[n] = lx[n] * ax[n]
+  
+  Tx    = Lx_to_Tx(Lx)
+  ex    = Txlx_to_ex(Tx,lx)
+  
+  # ex = ax in open interval
+  ex[n] = ax[n]
+  
+  out <- tibble(age, 
+                mx, 
+                ax, 
+                qx, 
+                lx, 
+                dx, 
+                Lx, 
+                Tx, 
+                ex)
+  return(out)
+}
+
+my_lifetable_tibble <- function(data_chunk){
+  
+  # create the 3 vectors needed by our first lifetable function
+  age <- data_chunk |> pull(age)
+  mx  <- data_chunk |> pull(mx)
+  ax  <- data_chunk |> pull(ax)
+  
+  # now apply our first version of the function
+  out <- my_lifetable(age, 
+                      mx, 
+                      ax )
+  return(out)
+}
